0382e2606e5e93295f632df0dbd4c2810474171d
Aditya <77260113+Raghav-45@users.noreply.github.com>
2023-10-30 13:51:33 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button'
+import { useStore } from '@/contexts/ContextApi'
 import { FC, useState } from 'react'
 
 interface HomeProps {
@@ -7,11 +8,14 @@ interface HomeProps {
 
 const Home: FC<HomeProps> = ({}) => {
   const [count, setCount] = useState(0)
+  const { geoLoc } = useStore()
   return (
     <div className="p-4">
       <Button onClick={() => setCount((count) => count + 1)}>
         count is {count}
       </Button>
+      <p>Lat: {geoLoc.lat}</p>
+      <p>Lng: {geoLoc.lng}</p>
     </div>
   )
 }
